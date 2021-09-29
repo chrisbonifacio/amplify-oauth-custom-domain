cb11ef0bf41a80833c6bd20d5ca23d6830be74ef
Chris Bonifacio <christopher.bonifacio@gmail.com>
2021-09-29 13:03:20 -0400

    update redirect uri logic

--- src/index.js
+++ src/index.js
@@ -4,18 +4,44 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Amplify } from "aws-amplify";
-import awsconfig from "./aws-exports";
+import awsConfig from "./aws-exports";
 require("dotenv").config();
 
-awsconfig.oauth.redirectSignIn = "https://" + window.location.hostname;
-awsconfig.oauth.redirectSignOut = "https://" + window.location.hostname;
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+const [localRedirectSignIn, productionRedirectSignIn] =
+  awsConfig.oauth.redirectSignIn.split(",");
+
+const [localRedirectSignOut, productionRedirectSignOut] =
+  awsConfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+  ...awsConfig,
+  oauth: {
+    ...awsConfig.oauth,
+    redirectSignIn: isLocalhost
+      ? localRedirectSignIn
+      : productionRedirectSignIn,
+    redirectSignOut: isLocalhost
+      ? localRedirectSignOut
+      : productionRedirectSignOut,
+  },
+};
 
 // Testing OAuth with custom domain
 // Google
 
 Amplify.Logger.LOG_LEVEL = "DEBUG";
 
-Amplify.configure(awsconfig);
+Amplify.configure(updatedAwsConfig);
 
 ReactDOM.render(
   <React.StrictMode>
